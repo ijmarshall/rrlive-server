7aa0545ddd9a59568b3744c1e009ab81738e833a
Frank <frank.soboczenski@gmail.com>
2021-12-09 15:00:10 +0000

    updating settings.py example
    default template update to include urllib requirements and db_uri link update

--- app/settings.py.example.py
+++ app/settings.py.example.py
@@ -1,11 +1,11 @@
+import urllib.parse
 from pydantic import BaseSettings, Field, validator
-
 
 class Settings(BaseSettings):
     env: str = Field("prod", env="ENV")
     app_url: str = Field("http://127.0.0.1:8080", env="APP_URL")
     db_uri: str = Field(
-        "postgresql://trialstreamer:[PASSWORD]@localhost:5432/trialstreamer", env="DB_URI"
+        "postgresql://trialstreamer:" + urllib.parse.quote("PASSWORD") + "@localhost:5433/trialstreamer", env="DB_URI"
     )
     github_client_id: str = Field("[ADD GITHUB CLIENT ID HERE]", env="GITHUB_CLIENT_ID")
     github_client_secret: str = Field("[ADD GITHUB CLIENT SECRET HERE]", env="GITHUB_CLIENT_SECRET")
@@ -15,6 +15,5 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = '.env'
-
 
 settings = Settings()
